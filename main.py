b481b9b36232ad472e07ecfc3b7c0b9aa6170674
Hussein Kabbout <hka@localhost.localdomain>
2017-09-19 16:36:10 +0200

    removed the second lineEdit/button and added regular expression to the first lineEdit

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import sys
+import re
 from PyQt5.QtCore import *
 from PyQt5.QtWidgets import *
 from PyQt5.QtQml import *
@@ -7,15 +8,12 @@ from PyQt5.QtQuickWidgets import *
 
 def val(self):
     s = search.text()
+    f = re.split(r',*', s)
+    a = f[0]
+    b = f[1]
     ctx = view.rootContext()
-    ctx.setContextProperty("g", s)
-    view.rootContext
-
-
-def vall(self):
-    s = ssearch.text()
-    ctx = view.rootContext()
-    ctx.setContextProperty("f", s)
+    ctx.setContextProperty("g", a)
+    ctx.setContextProperty("f", b)
     view.rootContext
 
 
@@ -25,12 +23,9 @@ if __name__ == '__main__':
     myApp = QApplication(sys.argv)
     # create search option and Button
     search = QLineEdit()
-    searchButton = QPushButton("Search for X Coordinate")
+    searchButton = QPushButton("Search for coordinates")
     searchButton.clicked.connect(val)
 
-    ssearch = QLineEdit()
-    ssearchButton = QPushButton("Search for Y Coordinate")
-    ssearchButton.clicked.connect(vall)
     # Create Layout
     window = QWidget()
     window.setLayout(QVBoxLayout())
@@ -42,8 +37,6 @@ if __name__ == '__main__':
     # Add widgets to layout
     window.layout().addWidget(search)
     window.layout().addWidget(searchButton)
-    window.layout().addWidget(ssearch)
-    window.layout().addWidget(ssearchButton)
     window.layout().addWidget(view)
     window.setMinimumSize(500, 500)
     # Show the Layout
