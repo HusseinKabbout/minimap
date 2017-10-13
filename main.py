4ee0e1cad20a6a2957c3a5fd32d01987324c22bb
Hussein Kabbout <hussein.kabbout@sourcepole.ch>
2017-10-13 13:41:30 +0200

    restructured code

--- main.py
+++ main.py
@@ -10,162 +10,175 @@ from pain import *
 import xml.etree.ElementTree as ET
 
 
-def selectColor(comboBox, rootObject):
-    selected = comboBox.currentText()
-    paintObject = rootObject.findChild(QObject, "paint")
-    paintObject.setProperty("lineColor", selected)
+class Minimap():
+    def __init__(self):
+        self.setupUi()
+        self.connect()
+        self.createLayout()
+        self.createQml()
+        self.addWidgets()
+        self.createColors()
 
+    def setupUi(self):
+        self.searchLabel = QLabel("Search for coordinate")
+        self.searchLabel.setFixedWidth(160)
+        self.lineEdit = QLineEdit()
+        self.lineEdit.setFixedWidth(285)
+        self.searchButton = QPushButton()
+        self.searchButton.setFixedWidth(30)
+        self.searchButton.resize(self.searchButton.sizeHint())
+        icon = QIcon()
+        icon.addPixmap(QPixmap(
+            "icons/searchIcon.png"), QIcon.Normal, QIcon.Off)
+        self.searchButton.setIcon(icon)
+        self.listLabel = QLabel("Import XML-coordinates \nand pin them on Map")
+        self.listLabel.setFixedWidth(160)
+        self.listEdit = QLineEdit()
+        self.listEdit.setFixedWidth(285)
+        self.listButton = QPushButton()
+        self.searchButton.setFixedWidth(30)
+        self.listButton.resize(self.listButton.sizeHint())
+        icon = QIcon()
+        icon.addPixmap(QPixmap(
+            "icons/listsearchIcon.png"), QIcon.Normal, QIcon.Off)
+        self.listButton.setIcon(icon)
+        self.colorLabel = QLabel("Change color of Line")
+        self.colorLabel.setFixedWidth(160)
+        self.comboBox = QComboBox()
+        self.comboBox.setFixedWidth(285)
+        self.selectColorBtn = QPushButton()
+        self.selectColorBtn.resize(self.selectColorBtn.sizeHint())
+        icon = QIcon()
+        icon.addPixmap(QPixmap(
+            "icons/changecolorIcon.png"), QIcon.Normal, QIcon.Off)
+        self.selectColorBtn.setIcon(icon)
 
-def openList(listEdit, rootObject):
-    fileName = QFileDialog.getOpenFileName(
-        None, "Open", "/home", "Only Xml(*.xml)")
-    listEdit.setText(fileName[0])
-    url = listEdit.text()
-    try:
-        XML = url
-        parXml = untangle.parse(XML)
-        for item in parXml.items.item:
-            titl = item.title.cdata
-            lati = item.latitude.cdata
-            longi = item.longitude.cdata
-            model.addMarker(
-                MarkerItem(QPointF(float(lati), float(longi)), titl))
-        mapObject = rootObject.findChild(QObject, "mapboxgl")
-        mapObject.setProperty("zoomLevel", 2)
-    except Exception:
+    def connect(self):
+        self.searchButton.clicked.connect(self.search)
+        self.lineEdit.returnPressed.connect(self.search)
+        self.listButton.clicked.connect(self.openList)
+        self.selectColorBtn.clicked.connect(self.selectColor)
+
+    def createLayout(self):
+        self.window = QWidget()
+        self.window.setLayout(QVBoxLayout())
+        self.controlS = QWidget()
+        self.controlS.setLayout(QHBoxLayout())
+        self.controlS.setMaximumSize(495, 50)
+        self.controlX = QWidget()
+        self.controlX.setLayout(QHBoxLayout())
+        self.controlX.setMaximumSize(495, 50)
+        self.jsonLine = QWidget()
+        self.jsonLine.setLayout(QHBoxLayout())
+        self.jsonLine.setMaximumSize(495, 50)
+
+    def addWidgets(self):
+        self.controlS.layout().addWidget(self.searchLabel)
+        self.controlS.layout().addWidget(self.lineEdit)
+        self.controlS.layout().addWidget(self.searchButton)
+        self.controlX.layout().addWidget(self.listLabel)
+        self.controlX.layout().addWidget(self.listEdit)
+        self.controlX.layout().addWidget(self.listButton)
+        self.jsonLine.layout().addWidget(self.colorLabel)
+        self.jsonLine.layout().addWidget(self.comboBox)
+        self.jsonLine.layout().addWidget(self.selectColorBtn)
+        self.window.layout().addWidget(self.controlS)
+        self.window.layout().addWidget(self.controlX)
+        self.window.layout().addWidget(self.jsonLine)
+        self.window.layout().addWidget(self.view)
+        self.window.setMinimumSize(500, 500)
+        self.window.setWindowTitle("Minimap")
+        self.window.show()
+
+    def createQml(self):
+        self.view = QQuickWidget()
+        self.model = MarkerModel()
+        self.context = self.view.rootContext()
+        self.context.setContextProperty('markerModel', self.model)
+        self.view.setSource(QUrl('map.qml'))
+        self.view.setMinimumSize(200, 200)
+        self.view.setResizeMode(self.view.SizeRootObjectToView)
+        self.rootObject = self.view.rootObject()
+
+    def createColors(self):
+        self.colors = []
+        self.colors.append("blue")
+        self.colors.append("red")
+        self.colors.append("yellow")
+        self.colors.append("black")
+        self.colors.append("white")
+        self.comboBox.addItems(self.colors)
+
+    def selectColor(self):
+        self.selected = self.comboBox.currentText()
+        paintObject = self.rootObject.findChild(QObject, "paint")
+        paintObject.setProperty("lineColor", self.selected)
+
+    def openList(self):
+        fileName = QFileDialog.getOpenFileName(
+            None, "Open", "/home", "Only Xml(*.xml)")
+        self.listEdit.setText(fileName[0])
+        url = self.listEdit.text()
         try:
-            i = 0
-            titl = []
-            tree = ET.parse(url)
-            root = tree.getroot()
-            for elem in root.findall('.//{'
-                                     'http://ogr.maptools.org/}Name'):
-                titl.append(elem.text)
-            for elem in root.findall('.//{'
-                                     'http://www.opengis.net/gml}coordinates'):
-                coord = []
-                coord.append(elem.text)
-                for elem in coord:
-                    part = elem.split(",")
-                    lati = part[0]
-                    longi = part[1]
-                    model.addMarker(
-                        MarkerItem(QPointF(
-                            float(lati), float(longi)), titl[i]))
-                    mapObject = rootObject.findChild(QObject, "mapboxgl")
-                    mapObject.setProperty("zoomLevel", 2)
-                    i = i + 1
+            XML = url
+            parXml = untangle.parse(XML)
+            for item in parXml.items.item:
+                titl = item.title.cdata
+                lati = item.latitude.cdata
+                longi = item.longitude.cdata
+                self.model.addMarker(
+                    MarkerItem(QPointF(float(lati), float(longi)), titl))
+            self.mapObject = self.rootObject.findChild(QObject, "mapboxgl")
+            self.mapObject.setProperty("zoomLevel", 2)
         except Exception:
-            pass
+            try:
+                i = 0
+                titl = []
+                tree = ET.parse(url)
+                root = tree.getroot()
+                for elem in root.findall('.//{'
+                                         'http://ogr.maptools.org/}Name'):
+                    titl.append(elem.text)
+                for elem in root.findall('.//{'
+                                         'http://www.opengis.net/gml}'
+                                         'coordinates'):
+                    coord = []
+                    coord.append(elem.text)
+                    for elem in coord:
+                        part = elem.split(",")
+                        lati = part[0]
+                        longi = part[1]
+                        self.model.addMarker(
+                            MarkerItem(QPointF(
+                                float(lati), float(longi)), titl[i]))
+                        self.mapObject = self.rootObject.findChild(
+                            QObject, "mapboxgl")
+                        self.mapObject.setProperty("zoomLevel", 2)
+                        i = i + 1
+            except Exception:
+                pass
+
+    def search(self):
+        try:
+            f = re.match(
+                r'^([+-]?\d+\.?\d*),?\s+([+-]?\d+\.?\d*)$',
+                self.lineEdit.text())
+            if abs(float(f[1])) <= 90 and abs(float(f[2])) <= 180:
+                lat = float(f[1])
+                lon = float(f[2])
+            self.mapObject = self.rootObject.findChild(QObject, "mapboxgl")
+            self.markerObject = self.rootObject.findChild(QObject, "marker")
+            self.mapObject.setProperty("lat", lat)
+            self.mapObject.setProperty("lon", lon)
+            self.markerObject.setProperty("coordinate", QGeoCoordinate(
+                lat, lon))
+            self.markerObject.setProperty("visible", True)
+        except Exception:
+            QMessageBox.warning(
+                None, "Invalid Coordinate", "Invalid coordinate")
+            self.lineEdit.selectAll()
 
 
-def search(lineEdit, rootObject):
-    try:
-        f = re.match(
-            r'^([+-]?\d+\.?\d*),?\s+([+-]?\d+\.?\d*)$', lineEdit.text())
-        if abs(float(f[1])) <= 90 and abs(float(f[2])) <= 180:
-            lat = float(f[1])
-            lon = float(f[2])
-        mapObject = rootObject.findChild(QObject, "mapboxgl")
-        markerObject = rootObject.findChild(QObject, "marker")
-        mapObject.setProperty("lat", lat)
-        mapObject.setProperty("lon", lon)
-        markerObject.setProperty("coordinate", QGeoCoordinate(lat, lon))
-        markerObject.setProperty("visible", True)
-    except Exception:
-        QMessageBox.warning(None, "Invalid Coordinate", "Invalid coordinate")
-        lineEdit.selectAll()
-
-
-# Main Function
-if __name__ == '__main__':
-    # Create main app
-    myApp = QApplication(sys.argv)
-    # create color_list
-    colors = []
-    colors.append("blue")
-    colors.append("red")
-    colors.append("yellow")
-    colors.append("black")
-    colors.append("white")
-    # create gui
-    searchLabel = QLabel("Search for coordinate")
-    searchLabel.setFixedWidth(160)
-    lineEdit = QLineEdit()
-    lineEdit.setFixedWidth(285)
-    searchButton = QPushButton()
-    searchButton.setFixedWidth(30)
-    searchButton.resize(searchButton.sizeHint())
-    icon = QIcon()
-    icon.addPixmap(QPixmap(
-        "icons/searchIcon.png"), QIcon.Normal, QIcon.Off)
-    searchButton.setIcon(icon)
-    listLabel = QLabel("Import XML-coordinates \nand pin them on Map")
-    listLabel.setFixedWidth(160)
-    listEdit = QLineEdit()
-    listEdit.setFixedWidth(285)
-    listButton = QPushButton()
-    searchButton.setFixedWidth(30)
-    listButton.resize(listButton.sizeHint())
-    icon = QIcon()
-    icon.addPixmap(QPixmap(
-        "icons/listsearchIcon.png"), QIcon.Normal, QIcon.Off)
-    listButton.setIcon(icon)
-    colorLabel = QLabel("Change color of Line")
-    colorLabel.setFixedWidth(160)
-    comboBox = QComboBox()
-    comboBox.setFixedWidth(285)
-    comboBox.addItems(colors)
-    selectColorBtn = QPushButton()
-    selectColorBtn.resize(selectColorBtn.sizeHint())
-    icon = QIcon()
-    icon.addPixmap(QPixmap(
-        "icons/changecolorIcon.png"), QIcon.Normal, QIcon.Off)
-    selectColorBtn.setIcon(icon)
-    # Create Layout
-    window = QWidget()
-    window.setLayout(QVBoxLayout())
-    controlS = QWidget()
-    controlS.setLayout(QHBoxLayout())
-    controlS.setMaximumSize(495, 50)
-    controlX = QWidget()
-    controlX.setLayout(QHBoxLayout())
-    controlX.setMaximumSize(495, 50)
-    jsonLine = QWidget()
-    jsonLine.setLayout(QHBoxLayout())
-    jsonLine.setMaximumSize(495, 50)
-    # Create Qml reference and create new Model
-    view = QQuickWidget()
-    model = MarkerModel()
-    context = view.rootContext()
-    context.setContextProperty('markerModel', model)
-    view.setSource(QUrl('map.qml'))
-    view.setMinimumSize(200, 200)
-    view.setResizeMode(view.SizeRootObjectToView)
-    rootObject = view.rootObject()
-    # Add widgets to layout
-    controlS.layout().addWidget(searchLabel)
-    controlS.layout().addWidget(lineEdit)
-    controlS.layout().addWidget(searchButton)
-    controlX.layout().addWidget(listLabel)
-    controlX.layout().addWidget(listEdit)
-    controlX.layout().addWidget(listButton)
-    jsonLine.layout().addWidget(colorLabel)
-    jsonLine.layout().addWidget(comboBox)
-    jsonLine.layout().addWidget(selectColorBtn)
-    window.layout().addWidget(controlS)
-    window.layout().addWidget(controlX)
-    window.layout().addWidget(jsonLine)
-    window.layout().addWidget(view)
-    window.setMinimumSize(500, 500)
-    # Connect search slot
-    searchButton.clicked.connect(lambda: search(lineEdit, rootObject))
-    lineEdit.returnPressed.connect(lambda: search(lineEdit, rootObject))
-    listButton.clicked.connect(lambda: openList(listEdit, rootObject))
-    selectColorBtn.clicked.connect(lambda: selectColor(comboBox, rootObject))
-    # Show the Layout
-    window.setWindowTitle("Minimap")
-    window.show()
-    # Execute the Application and Exit
-    myApp.exec_()
-    sys.exit()
+myApp = QApplication(sys.argv)
+GUI = Minimap()
+sys.exit(myApp.exec_())
